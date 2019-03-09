27d27d6ff40e66f10df6b91763432fbe72401354
val-gs <valblog.7321@gmail.com>
2019-03-10 04:25:49 +0900

    10-2-2 end

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:michael)
+    @user       = users(:michael)
+    @other_user = users(:archer)
   end
 
   test "should get new" do
